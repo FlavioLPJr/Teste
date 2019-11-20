6a0083a7c881bd50a363146fd29ea8f1f9cf2b2b
FlavioLPJr <48737347+FlavioLPJr@users.noreply.github.com>
2019-11-20 15:20:13 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 (async () => {
 const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
-  await page.goto('www.google.com.br');
+  await page.goto('https://google.com/');
   await page.waitFor(5000);
   await page.screenshot({path: 'google.png'});
  
